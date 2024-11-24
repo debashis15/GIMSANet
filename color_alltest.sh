053774125bb2e1a974c3fe663bdf7ab002105fd3
Debashis Das <55309072+debashis15@users.noreply.github.com>
2024-11-24 17:10:46 +0530

    Add files via upload

---
+++ color_alltest.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#python3 Test_Synthetic_Coloured.py --noise 30 --weightsPath './training_checkpoints/ckpt_045' --epoch 45
+#python3 Test_Synthetic_Coloured.py --noise 30 --weightsPath './training_checkpoints/ckpt_050' --epoch 50
+python3 Test_Synthetic_Coloured.py --noise 30 --weightsPath './training_checkpoints/ckpt_055' --epoch 55
+#python3 Test_Synthetic_Coloured.py --noise 30 --weightsPath './training_checkpoints/ckpt_060' --epoch 60
+
+#python3 Test_Synthetic_Coloured.py --noise 40 --weightsPath './training_checkpoints/ckpt_045' --epoch 45
+#python3 Test_Synthetic_Coloured.py --noise 40 --weightsPath './training_checkpoints/ckpt_050' --epoch 50
+python3 Test_Synthetic_Coloured.py --noise 40 --weightsPath './training_checkpoints/ckpt_055' --epoch 55
+#python3 Test_Synthetic_Coloured.py --noise 40 --weightsPath './training_checkpoints/ckpt_060' --epoch 60
+
+#python3 Test_Synthetic_Coloured.py --noise 50 --weightsPath './training_checkpoints/ckpt_045' --epoch 45
+#python3 Test_Synthetic_Coloured.py --noise 50 --weightsPath './training_checkpoints/ckpt_050' --epoch 50
+python3 Test_Synthetic_Coloured.py --noise 50 --weightsPath './training_checkpoints/ckpt_055' --epoch 55
+#python3 Test_Synthetic_Coloured.py --noise 50 --weightsPath './training_checkpoints/ckpt_060' --epoch 60
+
+#python3 Test_Synthetic_Coloured.py --noise 60 --weightsPath './training_checkpoints/ckpt_045' --epoch 45
+#python3 Test_Synthetic_Coloured.py --noise 60 --weightsPath './training_checkpoints/ckpt_050' --epoch 50
+python3 Test_Synthetic_Coloured.py --noise 60 --weightsPath './training_checkpoints/ckpt_055' --epoch 55
+#python3 Test_Synthetic_Coloured.py --noise 60 --weightsPath './training_checkpoints/ckpt_060' --epoch 60
